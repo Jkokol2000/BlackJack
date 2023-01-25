fbb97885c8e1f4c1c7dc27b2fcd1f0ca521e0f30
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-01-25 10:51:53 -0800

    Removed all console.logs and proofread all the code.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
+/*jshint esversion: 6 */
+
 /* Constants */
 const SUITICONS = {
     'spades': 's',
     'hearts': 'h',
     'diamonds': 'd',
     'clubs': 'c'
-}
+};
 
 /* State Variables */
-let deck
+let deck;
 let winner;
-let inDebt;
 let playerHand;
 let dealerHand;
-let currentBet
-let currentWinnings
-let currentChips
+let currentBet;
+let currentWinnings;
+let currentChips;
 let playerScore;
 let dealerScore;
 let numberOfGames = 0;
@@ -29,7 +30,7 @@ let playerCard = {
     '6': document.querySelector('.card-image-6'),
     '7': document.querySelector('.card-image-7'),
     '8': document.querySelector('.card-image-8')
-}
+};
 let dealerCard = {
     '1': document.querySelector('.dcard-image-1'),
     '2': document.querySelector('.dcard-image-2'),
@@ -39,10 +40,8 @@ let dealerCard = {
     '6': document.querySelector('.dcard-image-6'),
     '7': document.querySelector('.dcard-image-7'),
     '8': document.querySelector('.dcard-image-8'),
-}
-let dealing = document.querySelector('.deal')
+};
 let dealButton = document.querySelector('.deal-button');
-let currentBetEl = document.querySelector('.bet-amount');
 let currentWinningsEl = document.querySelector('.winnings');
 let messageEl = document.querySelector('.message');
 let standButton = document.querySelector('.stand-button');
@@ -56,26 +55,26 @@ let loanButton = document.querySelector('.loan-button');
 dealButton.addEventListener("click", function () {
     currentBet = parseInt(document.querySelector(".bet-number").value);
     deal();
-})
+});
 standButton.addEventListener("click", calcPlayerWins);
 hitButton.addEventListener("click", hit);
 nextHand.addEventListener("click", function () {
     numberOfGames++;
     init();
-})
+});
 loanButton.addEventListener("click", function () {
-    currentChips += 1000
-    currentWinnings -= 1000
-    switchButtonVisability(loanButton, "hidden")
+    currentChips += 1000;
+    currentWinnings -= 1000;
+    switchButtonVisability(loanButton, "hidden");
     init();
-})
+});
 
 /* Functions and Classes */
 //Card class that defines how the cards will be setup in the deck
 class Card {
     constructor(rank, suit) {
-        this.rank = rank
-        this.suit = suit
+        this.rank = rank;
+        this.suit = suit;
     }
 }
 // Deck class that allows me to create a deck, shuffle it, and deal the cards
@@ -106,9 +105,9 @@ class Deck {
 }
 
 init();
-switchButtonVisability(hitButton, "hidden")
-switchButtonVisability(standButton, "hidden")
-switchButtonVisability(loanButton, "hidden")
+switchButtonVisability(hitButton, "hidden");
+switchButtonVisability(standButton, "hidden");
+switchButtonVisability(loanButton, "hidden");
 function init() {
     currentBet = 0;
     deck = new Deck();
@@ -122,15 +121,15 @@ function init() {
     switchButtonVisability(nextHand, "hidden");
     renderScore(playerScore);
     if (numberOfGames !== 0 && currentChips > 0) {
-        render("Place a bet.")
+        render("Place a bet.");
         switchButtonVisability(dealButton, "visible");
     } else if (currentChips === 0) {
-        render("You are out of chips! Want a loan?")
+        render("You are out of chips! Want a loan?");
         switchButtonVisability(loanButton, "visible");
     } else {
         currentWinnings = 0;
         currentChips = 1000;
-        render("Welcome to BlackJack! <br> Place a bet.")
+        render("Welcome to BlackJack! <br> Place a bet.");
     }
 }
 
@@ -142,34 +141,29 @@ function deal() {
     switchButtonVisability(standButton, "visible");
     switchButtonVisability(dealButton, "hidden");
     if (currentBet == 0) {
-        renderMessage("You must bet more than 0 dollars!")
+        renderMessage("You must bet more than 0 dollars!");
         switchButtonVisability(hitButton, "hidden");
         switchButtonVisability(standButton, "hidden");
         switchButtonVisability(dealButton, "visible");
     } else if (currentBet > currentChips) {
-        renderMessage("You cannot bet more than what you have!")
+        renderMessage("You cannot bet more than what you have!");
         switchButtonVisability(hitButton, "hidden");
         switchButtonVisability(standButton, "hidden");
         switchButtonVisability(dealButton, "visible");
     } else {
-        console.log(currentBet);
         currentWinnings -= currentBet;
-        console.log(currentWinnings)
-        console.log(currentBet)
         currentChips -= currentBet;
-        console.log(currentChips)
-        console.log(currentBet);
-        playerHand = deck.dealCard(2)
-        dealerHand = deck.dealCard(2)
-        playerCard[1].classList.add(`card`, `${SUITICONS[playerHand[0].suit]}${playerHand[0].rank}`)
-        playerCard[2].classList.add(`card`, `${SUITICONS[playerHand[1].suit]}${playerHand[1].rank}`)
-        dealerCard[2].classList.add(`card`, `${SUITICONS[dealerHand[1].suit]}${dealerHand[1].rank}`)
+        playerHand = deck.dealCard(2);
+        dealerHand = deck.dealCard(2);
+        playerCard[1].classList.add(`card`, `${SUITICONS[playerHand[0].suit]}${playerHand[0].rank}`);
+        playerCard[2].classList.add(`card`, `${SUITICONS[playerHand[1].suit]}${playerHand[1].rank}`);
+        dealerCard[2].classList.add(`card`, `${SUITICONS[dealerHand[1].suit]}${dealerHand[1].rank}`);
         dealerCard[1].classList.add(`card`, `back`);
         renderMessage("");
-        calcCurrentScore("P")
-        renderScore(playerScore)
+        calcCurrentScore("P");
+        renderScore(playerScore);
         if (playerScore >= 21 ) {
-            calcPlayerWins()
+            calcPlayerWins();
         } else {
             return;
         }
@@ -181,26 +175,22 @@ function calcPlayerWins() {
     if (playerScore <= 21) {
         dealerDeals(dealerScore);
         if (playerScore === dealerScore) {
-            winner = 'T'
-            currentWinnings += currentBet
-            console.log(currentWinnings)
-            console.log(currentBet)
-            currentChips += currentBet
-            console.log(currentChips, "This is after a tie")
-            console.log(currentBet)
-            renderMessage("Push! Here's your money back.")
+            winner = 'T';
+            currentWinnings += currentBet;
+            currentChips += currentBet;
+            renderMessage("Push! Here's your money back.");
         } else if (playerScore > dealerScore || dealerScore > 21 || (playerScore === 21 && dealerScore !== 21)) {
-            winner = 'P'
-            currentWinnings += currentBet * 2
-            currentChips += currentBet * 2
-            renderMessage(`You win! You won ${currentBet * 2}$`)
+            winner = 'P';
+            currentWinnings += currentBet * 2;
+            currentChips += currentBet * 2;
+            renderMessage(`You win! You won ${currentBet * 2}$`);
         } else {
-            winner = 'D'
-            renderMessage(`Dealer Wins! You lost ${currentBet}$`)
+            winner = 'D';
+            renderMessage(`Dealer Wins! You lost ${currentBet}$`);
         }
     } else {
-        winner = 'D'
-        renderMessage(`You Bust! You lost ${currentBet}$`)
+        winner = 'D';
+        renderMessage(`You Bust! You lost ${currentBet}$`);
     }
     switchButtonVisability(hitButton, "hidden");
     switchButtonVisability(standButton, "hidden");
@@ -209,76 +199,67 @@ function calcPlayerWins() {
 
 
 function calcScore(rank) {
-    score = 0
+    let score = 0;
     switch (rank) {
         case 'A':
-            score += 1
+            score += 1;
             break;
         case '02':
-            score += 2
+            score += 2;
             break;
         case '03':
-            score += 3
+            score += 3;
             break;
         case '04':
-            score += 4
+            score += 4;
             break;
         case '05':
-            score += 5
+            score += 5;
             break;
         case '06':
-            score += 6
+            score += 6;
             break;
         case '07':
-            score += 7
+            score += 7;
             break;
         case '08':
-            score += 8
+            score += 8;
             break;
         case '09':
-            score += 9
+            score += 9;
             break;
         case '10':
-            score += 10
+            score += 10;
             break;
         case 'J':
-            score += 11
+            score += 11;
             break;
         case 'Q':
-            score += 12
+            score += 12;
             break;
         case 'K':
-            score += 13
+            score += 13;
             break;
     }
-    return score
+    return score;
 }
 
 function renderMessage(message) {
     messageEl.innerHTML = message;
     currentWinningsEl.innerHTML = `Current Winnings: ${currentWinnings}$`;
-    currentChipsEl.innerHTML = `Current Chips: ${currentChips}$`
-}
-
-function renderCards() {
-    playerCard[1].classList.add(`card`, `${SUITICONS[playerHand[0].suit]}${playerHand[0].rank}`)
-    playerCard[2].classList.add(`card`, `${SUITICONS[playerHand[1].suit]}${playerHand[1].rank}`)
-    dealerCard[2].classList.add(`card`, `${SUITICONS[dealerHand[1].suit]}${dealerHand[1].rank}`)
-    dealerCard[1].classList.add(`card`, `${SUITICONS[dealerHand[0].suit]}${dealerHand[0].rank}`)
-    calcCurrentScore("P");
-    calcCurrentScore("D");
+    currentChipsEl.innerHTML = `Current Chips: ${currentChips}$`;
 }
 
 function revealFacedown() {
-    dealerCard[1].classList.remove('back')
-    dealerCard[1].classList.add(`card`, `${SUITICONS[dealerHand[0].suit]}${dealerHand[0].rank}`)
+    dealerCard[1].classList.remove('back');
+    dealerCard[1].classList.add(`card`, `${SUITICONS[dealerHand[0].suit]}${dealerHand[0].rank}`);
 }
 function hit() {
-    cardToHit = deck.dealCard(1)
-    playerHand.push(cardToHit[0])
+    let cardToHit = deck.dealCard(1);
+    playerHand.push(cardToHit[0]);
     calcCurrentScore("P");
-    renderScore(playerScore)
-    playerCard[playerHand.length].classList.add(`card`, `${SUITICONS[playerHand[playerHand.length - 1].suit]}${playerHand[playerHand.length - 1].rank}`)
+    renderScore(playerScore);
+    playerCard[playerHand.length].classList.add(`card`, `${SUITICONS[playerHand[playerHand.length - 1].suit]}${playerHand[playerHand.length - 1].rank}`);
     if (playerScore >= 21) {
         calcPlayerWins();
     }
@@ -291,39 +272,37 @@ function render(message) {
 }
 
 function resetBoard() {
-    for (element in playerCard) {
-        playerCard[element].className = ''
-        playerCard[element].classList.add(`card-image-${element}`)
+    for (let element in playerCard) {
+        playerCard[element].className = '';
+        playerCard[element].classList.add(`card-image-${element}`);
     }
-    for (element in dealerCard) {
-        dealerCard[element].className = ''
-        dealerCard[element].classList.add(`dcard-image-${element}`)
+    for (let delement in dealerCard) {
+        dealerCard[delement].className = '';
+        dealerCard[delement].classList.add(`dcard-image-${delement}`);
     }
 
 }
 
 function calcCurrentScore(PorD) {
     if (PorD === "D") {
-        dealerScore = 0
-        for (i = 0; i < dealerHand.length; i++) {
-            dealerScore += calcScore(dealerHand[i].rank)
+        dealerScore = 0;
+        for (let i = 0; i < dealerHand.length; i++) {
+            dealerScore += calcScore(dealerHand[i].rank);
         }
     } else if (PorD === "P") {
-        playerScore = 0
-        for (i = 0; i < playerHand.length; i++) {
-            playerScore += calcScore(playerHand[i].rank)
+        playerScore = 0;
+        for (let i = 0; i < playerHand.length; i++) {
+            playerScore += calcScore(playerHand[i].rank);
         }
-    } else {
-        return console.log("Neither Player nor Dealer given")
-    }
+}
 }
 function dealerDeals() {
     calcCurrentScore("D");
-    for (i = 0; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
         if (dealerScore < 17) {
-            cardToHit = deck.dealCard(1)
+            let cardToHit = deck.dealCard(1);
             dealerHand.push(cardToHit[0]);
-            dealerCard[dealerHand.length].classList.add(`card`, `${SUITICONS[dealerHand[dealerHand.length - 1].suit]}${dealerHand[dealerHand.length - 1].rank}`)
+            dealerCard[dealerHand.length].classList.add(`card`, `${SUITICONS[dealerHand[dealerHand.length - 1].suit]}${dealerHand[dealerHand.length - 1].rank}`);
             calcCurrentScore("D");
         } else {
             break;
