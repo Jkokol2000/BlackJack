b8a9953f71ef09b36bda960226956374fe88d5aa
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-01-27 02:19:45 -0800

    Make dealer winning message clearer, make renderScore more consise, and add psudocode

---
+++ js/pseudocode.js
@@ -0,0 +1,22 @@
+//Define Important Constants:
+//The deck size (likely a variable) 
+//The value in which the player/dealer will bust
+//The value of an ace (either low or high)
+//The value where the dealer will continuously hit until they bust (17)
+//How much the player will win in terms of chips
+//Define Important Variables:
+//The Player’s and the Dealer’s Hand
+//The Player’s or Dealer’s Bust status (true or false)
+//The Cards currently in deck (Deck will likely be a object with two lists representing suits and card values)
+//The player’s Wager and current Balance
+//Initialize the board and define the game rules:
+//Create a board with the deck in the middle, then allow the player to bet
+//The deck is shuffled, then the dealer Deals the cards to player, then dealer, then player, then dealer
+//Show the player’s cards, and one of the dealer’s cards
+//The player will be able to choose between hitting or staying
+//If the player busts, they immediately lose
+//If the player stays, the dealer reveals their other card, then takes cards until their hand equals 17 or more
+//If the dealer busts or has a hand worse than the player’s, the player wins
+//If the dealer has a better hand or blackjack, the dealer wins
+//If the hand’s values are equal, a “push” happens (essentially a tie)
+//The cards are then retrieved, the deck is shuffled, and the player is able to choose to bet again to start a new game. If their balance is 0, they are able to click a “loan” button, giving them more chips but reducing their “winnings” by a considerable amount.
